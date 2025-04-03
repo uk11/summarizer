83590608bf6ca40df49764f99d58d9865f924376
uk11 <dlckddnrr@gmail.com>
2025-04-03 14:22:20 +0900

    feat: add upload fetch

---
+++ src/fetch/index.ts
@@ -0,0 +1,13 @@
+export const uploadAndSummary = async (file: File) => {
+  const formData = new FormData();
+  formData.append('file', file);
+
+  const res = await fetch('/api/summary', {
+    method: 'POST',
+    body: formData,
+  });
+
+  if (!res.ok) throw new Error('요약 실패');
+
+  return res.json();
+};
