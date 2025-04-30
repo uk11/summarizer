59dee342b818a9a5bb65cc325a57dd0f9ab7a4d2
uk11 <dlckddnrr@gmail.com>
2025-04-30 19:02:33 +0900

    feat: prisma 추가

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['pdf-parse'],
   },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
 };
 
 export default nextConfig;
