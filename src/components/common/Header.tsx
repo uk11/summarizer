9eb460da17a8b0dc6a196fff867200b7e5d96435
uk11 <dlckddnrr@gmail.com>
2025-04-30 19:01:43 +0900

    feat: 소셜로그인 ui 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,25 @@
+'use client';
+
 import Link from 'next/link';
+import LoginModal from '../pages/home/LoginModal';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import LogoutModal from '../pages/home/LogoutModal';
 
 export default function Header() {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+  const { data: session, status } = useSession();
+
+  const handleLoginModalClick = () => {
+    setIsLoginModalOpen(!isLoginModalOpen);
+  };
+
+  const handleLogoutModalClick = () => {
+    setIsLogoutModalOpen(!isLogoutModalOpen);
+  };
+
   return (
     <header className='h-[60px] flex items-center justify-between px-5 border-b shadow-sm bg-white sticky top-0 z-50'>
       <Link href='/' className='text-[20px] font-bold text-gray-800'>
@@ -12,13 +31,40 @@ export default function Header() {
           요약 내역
         </Link>
 
-        <Link
-          href='/test'
-          className='rounded px-3 py-1 border text-gray-600 hover:bg-gray-100'
-        >
-          로그인
-        </Link>
+        {status === 'loading' ? (
+          <div className='w-[30px] h-[30px] rounded-full bg-gray-200 animate-pulse' />
+        ) : status === 'authenticated' ? (
+          <Image
+            src={session.user.image!}
+            alt='프로필 이미지'
+            width={30}
+            height={30}
+            onClick={handleLogoutModalClick}
+            className='rounded-full cursor-pointer'
+          />
+        ) : (
+          <button
+            className='rounded px-3 py-1 border text-gray-600 hover:bg-gray-100 cursor-pointer'
+            onClick={handleLoginModalClick}
+          >
+            로그인
+          </button>
+        )}
       </div>
+
+      {isLoginModalOpen && (
+        <LoginModal
+          isLoginModalOpen={isLoginModalOpen}
+          handleLoginModalClick={handleLoginModalClick}
+        />
+      )}
+
+      {isLogoutModalOpen && (
+        <LogoutModal
+          isLogoutModalOpen={isLogoutModalOpen}
+          handleLogoutModalClick={handleLogoutModalClick}
+        />
+      )}
     </header>
   );
 }
---
+++ src/components/pages/home/LoginModal.tsx
@@ -0,0 +1,39 @@
+import { createPortal } from 'react-dom';
+import XSvg from '@/components/svg-components/XSvg';
+import { signIn } from 'next-auth/react';
+
+type Props = {
+  isLoginModalOpen: boolean;
+  handleLoginModalClick: () => void;
+};
+
+export default function LoginModal({
+  isLoginModalOpen,
+  handleLoginModalClick,
+}: Props) {
+  if (!isLoginModalOpen) return null;
+
+  return createPortal(
+    <div className='fixed inset-0 bg-black/40 flex justify-center items-center'>
+      <div className='w-[400px] bg-white flex justify-center items-center flex-col px-[20px] py-[30px] rounded-[10px]'>
+        <div className='relative w-full pb-[20px]'>
+          <h1 className='text-center text-lg font-semibold'>로그인</h1>
+          <button
+            onClick={handleLoginModalClick}
+            className='absolute right-0 top-0 cursor-pointer'
+          >
+            <XSvg />
+          </button>
+        </div>
+
+        <button
+          className='w-full border px-[10px] py-[8px] rounded-[10px] cursor-pointer'
+          onClick={() => signIn('google')}
+        >
+          Google 계정으로 계속하기
+        </button>
+      </div>
+    </div>,
+    document.body
+  );
+}
