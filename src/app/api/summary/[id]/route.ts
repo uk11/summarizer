f043c7b8b498ff8cbaa50745fb20fa369f49e3e1
uk11 <dlckddnrr@gmail.com>
2025-05-04 20:51:49 +0900

    fix: params id await 추가

--- src/app/api/summary/[id]/route.ts
+++ src/app/api/summary/[id]/route.ts
@@ -2,14 +2,15 @@ import { db } from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
 type Props = {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 };
 
-export async function DELETE(_: NextRequest, { params }: Props) {
+export async function DELETE(req: NextRequest, { params }: Props) {
+  const { id } = await params;
   try {
-    await db.summary.delete({ where: { id: params.id } });
+    await db.summary.delete({ where: { id } });
 
     return NextResponse.json({ success: true }, { status: 200 });
   } catch (err) {
@@ -18,4 +19,24 @@ export async function DELETE(_: NextRequest, { params }: Props) {
 
     return NextResponse.json({ error: message }, { status: 500 });
   }
+}
+
+export async function PATCH(req: NextRequest, { params }: Props) {
+  const { id } = await params;
+  const body = await req.json();
+  const { fileName } = body;
+
+  if (!id || !fileName) {
+    return NextResponse.json(
+      { error: 'id 또는 파일명이 없습니다.' },
+      { status: 400 }
+    );
+  }
+
+  await db.summary.update({
+    where: { id },
+    data: { fileName },
+  });
+
+  return NextResponse.json({ success: true }, { status: 200 });
 }
--- src/lib/summary.ts
+++ src/lib/summary.ts
@@ -1,4 +1,4 @@
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from './authOptions';
 import type { Summary } from '@prisma/client';
 import { getServerSession } from 'next-auth';
 import { getAnonymousId } from './auth';
