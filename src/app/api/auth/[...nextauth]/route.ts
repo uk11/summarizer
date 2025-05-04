d450157a2c0b7fd478b4ff072a61e5ec2887c0ad
uk11 <dlckddnrr@gmail.com>
2025-05-04 20:36:15 +0900

    fix: authOptions 분리

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,42 +1,5 @@
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import NextAuth, { type NextAuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import KakaoProvider from 'next-auth/providers/kakao';
-import { db } from '@/lib/prisma';
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(db),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-    }),
-    KakaoProvider({
-      clientId: process.env.AUTH_KAKAO_ID!,
-      clientSecret: process.env.AUTH_KAKAO_SECRET!,
-    }),
-  ],
-  session: {
-    strategy: 'jwt',
-    maxAge: 60 * 60 * 24,
-  },
-  callbacks: {
-    // NOTE: 로그인 시 유저 id를 토큰에 저장
-    jwt: async ({ token, user }) => {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    // NOTE: 토큰에 저장된 유저 id를 세션에 저장하고 추후 클라이언트에서 session.user.id로 사용자 구분
-    session: async ({ session, token }) => {
-      if (session.user && token.id) {
-        session.user.id = token.id as string;
-      }
-      return session;
-    },
-  },
-};
+import NextAuth from 'next-auth';
+import { authOptions } from '@/lib/authOptions';
 
 const handler = NextAuth(authOptions);
 
