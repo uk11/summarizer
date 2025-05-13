ae86bd9cd9aba2ced20f2679bffa19fb81ad9727
uk11 <dlckddnrr@gmail.com>
2025-05-13 22:26:12 +0900

    feat: 계정정보 모달 저장 목록 추가

--- src/components/common/Header/UserInfoModal.tsx
+++ src/components/common/Header/UserInfoModal.tsx
@@ -1,12 +1,15 @@
-import { createPortal } from 'react-dom';
 import XSvg from '@/components/svg-components/XSvg';
+import { createPortal } from 'react-dom';
 import { useSession } from 'next-auth/react';
 import { signOut } from 'next-auth/react';
 import { useOnClickOutside } from '@/hooks/useOutsideClick';
-import { useQuery } from '@tanstack/react-query';
-import { getSummaryAndChat } from '@/fetch';
-import { useParams } from 'next/navigation';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { formatDate } from '@/lib/day';
+import { deleteSummary, getSummary } from '@/fetch';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import DeleteModal from '../Sidebar/DeleteModal';
+import { useToast } from '@/hooks/useToast';
 
 type Props = {
   isOpen: boolean;
@@ -14,13 +17,15 @@ type Props = {
 };
 
 export default function UserInfoModal({ isOpen, onClose }: Props) {
-  const params = useParams();
-  const summaryId = params.id as string;
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const [currentId, setCurrentId] = useState<string | null>(null);
   const { data: session } = useSession();
+  const { showToast } = useToast();
 
-  const { data: summaryAndChatData } = useQuery({
-    queryKey: ['SummaryAndChat', summaryId],
-    queryFn: () => getSummaryAndChat(summaryId),
+  const { data: summaries } = useQuery({
+    queryKey: ['summaries'],
+    queryFn: getSummary,
   });
 
   const handleLogout = () => {
@@ -28,8 +33,24 @@ export default function UserInfoModal({ isOpen, onClose }: Props) {
     signOut({ callbackUrl: '/' });
   };
 
+  const handleNavigateToSummary = (id: string) => {
+    onClose();
+    router.push(`/result/${id}`);
+  };
+
+  const { mutate: deleteMutate } = useMutation({
+    mutationFn: deleteSummary,
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['summaries'] });
+      showToast('삭제되었습니다.', 'success');
+    },
+  });
+
   const { targetRef } = useOnClickOutside({
-    onClickOutside: onClose,
+    onClickOutside: () => {
+      if (!currentId) onClose();
+    },
   });
 
   if (!isOpen) return null;
@@ -42,7 +63,7 @@ export default function UserInfoModal({ isOpen, onClose }: Props) {
       >
         <div className='mb-[26px]'>
           <div className='w-full mb-[14px] flex justify-between items-center'>
-            <h1 className='font-semibold text-[18px]'>계정 정보</h1>
+            <h2 className='font-semibold text-[18px]'>계정 정보</h2>
             <button className='-translate-y-[2px]' onClick={onClose}>
               <XSvg />
             </button>
@@ -57,22 +78,58 @@ export default function UserInfoModal({ isOpen, onClose }: Props) {
         </div>
 
         <div>
-          <div className='font-semibold text-[18px] mb-[14px]'>
+          <h2 className='font-semibold text-[18px] mb-[14px]'>
             저장된 요약 및 채팅
-          </div>
-          <div className='flex w-[500px] border-b border-gray-300'>
+          </h2>
+
+          <div className='flex border-b border-gray-300 mb-[6px] pb-[6px]'>
             <div className='flex-1'>파일명</div>
             <div className='flex-1'>생성일</div>
           </div>
-          {summaryAndChatData &&
-            summaryAndChatData.data.map((data) => (
-              <div key={data.id} className='flex'>
-                <div className='flex-1'>{data.fileName}</div>
-                <div className='flex-1'>
-                  {formatDate(data.createdAt, 'YY년 MM월 DD일')}
-                </div>
-              </div>
-            ))}
+
+          <ul className='flex flex-col gap-[6px]'>
+            {summaries &&
+              summaries.data
+                .filter((item) => item.isSaved)
+                .map((data) => (
+                  <li key={data.id} className='flex'>
+                    <div className='flex-1 text-blue-600'>
+                      <button
+                        onClick={() => handleNavigateToSummary(data.id)}
+                        className='text-left'
+                      >
+                        {data.fileName}
+                      </button>
+                    </div>
+
+                    <div className='flex flex-1 justify-between items-center'>
+                      <div>{formatDate(data.createdAt, 'YY년 MM월 DD일')}</div>
+                      <button
+                        className='mr-[10px] text-red-500'
+                        onClick={() => {
+                          setCurrentId(data.id);
+                        }}
+                      >
+                        삭제
+                      </button>
+                    </div>
+
+                    {currentId === data.id && (
+                      <DeleteModal
+                        isOpen={true}
+                        fileName={data.fileName}
+                        onClose={() => {
+                          setCurrentId(null);
+                        }}
+                        onDelete={() => {
+                          deleteMutate(data.id);
+                          setCurrentId(null);
+                        }}
+                      />
+                    )}
+                  </li>
+                ))}
+          </ul>
         </div>
       </div>
     </div>,
