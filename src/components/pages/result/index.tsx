e6d60f8c1b2f9447cf6eda97b3c1c8f9e3a562e8
uk11 <dlckddnrr@gmail.com>
2025-05-22 00:27:23 +0900

    fix: 권한별로 접근할 수 있도록 수정

--- src/components/pages/result/index.tsx
+++ src/components/pages/result/index.tsx
@@ -6,18 +6,36 @@ import SummaryChat from './SummaryChat';
 import { getChatMessages } from '@/fetch';
 import { useQuery } from '@tanstack/react-query';
 import useBottomScroll from '@/hooks/useBottomScroll';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useToast } from '@/hooks/useToast';
+import { useEffect } from 'react';
 
 type Props = {
   summary: Summary;
 };
 
 export default function ResultClient({ summary }: Props) {
+  const router = useRouter();
+  const { showToast } = useToast();
+  const { data: session } = useSession();
+  const isAccess = summary.userId && session?.user?.id !== summary.userId;
+
   const { data: chatMessages } = useQuery({
     queryKey: ['summaryChat', summary.id],
     queryFn: () => getChatMessages(summary.id),
   });
 
   const { scrollRef } = useBottomScroll(chatMessages!);
+
+  useEffect(() => {
+    if (isAccess) {
+      router.replace('/');
+      showToast('권한이 없습니다.', 'error');
+    }
+  }, [isAccess, showToast, router]);
+
+  if (isAccess) return null;
 
   return (
     <div
