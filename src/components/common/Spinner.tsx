85872c7fb3e2693f2af1bed7882bd56950b6aa3c
uk11 <dlckddnrr@gmail.com>
2025-05-21 23:39:36 +0900

    fix: 스피너 z-index 변경

--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -1,6 +1,6 @@
 export default function Spinner() {
   return (
-    <div className='fixed inset-0 bg-black/20 flex items-center justify-center'>
+    <div className='fixed inset-0 bg-black/20 flex items-center justify-center z-[51]'>
       <div className='relative w-[12px] h-[12px] rounded-full mx-auto text-blue-400 upload-loading'></div>
     </div>
   );
