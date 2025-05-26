d4ae5b23274016ed569696d7387ae8ef66abb25f
uk11 <dlckddnrr@gmail.com>
2025-05-26 15:56:59 +0900

    fix: 메인페이지 간격 수정

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -3,10 +3,10 @@ import Upload from './Upload';
 export default function HomeClient() {
   return (
     <div className='flex flex-col items-center max-md:px-[16px]'>
-      <div className='flex flex-col items-center mt-[60px]'>
-        <h2 className='text-4xl font-bold mb-[20px]'>AI 요약 도구</h2>
+      <div className='flex flex-col items-center mt-[100px] max-md:mt-[70px]'>
+        <h2 className='text-4xl font-bold mb-[30px]'>AI 요약 도구</h2>
 
-        <p className='text-2xl max-md:text-xl'>
+        <p className='text-2xl max-md:text-xl mb-[50px]'>
           파일을 업로드하면 AI가 대신 요약해주고{' '}
           <span className='max-md:block'>
             채팅을 통해 빠르게 이해할 수 있습니다.
