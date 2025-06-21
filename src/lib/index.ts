6fe90e28e42b6919860011e923cdb16fbc6a8a93
uk11 <dlckddnrr@gmail.com>
2025-06-22 02:51:53 +0900

    feat: metadata 추가

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://summarizer.site'
+    : 'http://localhost:3000';
+
+export const PRODUCTION_URL = 'https://summarizer.site';
